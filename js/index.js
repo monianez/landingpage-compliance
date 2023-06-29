233d597fc513b7b3c92af5b9dca23c11996c4936
Mónica Añez Hernández <monianez@gmail.com>
2023-06-29 19:22:08 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const li-document.querySelectorAll('Header-li');
+const art-document.querySelectorAll('article');
+
+function activeMenu(){
+    let len-act-length;
+    while(--len && window.scrollY)
+}
